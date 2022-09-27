5833024eaff06ba135edb3c8709c38837a98764e
Gautam <Gautam.Shankar@yokogawa.com>
2022-09-27 10:55:19 +0530

    initial commit

---
+++ src/main/java/com/yokogawa/biomes/BioMesApplication.java
@@ -0,0 +1,13 @@
+package com.yokogawa.biomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioMesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioMesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yokogawa/biomes/BioMesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yokogawa.biomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioMesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
